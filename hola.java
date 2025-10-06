c97ec8b3cd9f1d4e403625b3c9f9daf3a6c9c08f
rolando choque <rolandochoqueparco099@gmail.com>
2025-10-05 19:10:34 -0500

    modifique hola.java

---
+++ hola.java
@@ -0,0 +1,7 @@
+
+public class Hola {
+    public static void main (String[] args){
+        System.out.println("hola mundo en java");
+        System.out.println("esta vez modificando repositorio");
+    }
+}
